05cea748eba23b63f73003fce701b0d9a3bfaac7
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-05-14 10:05:22 -0300

    Fazendo a estruturação do HTML + CSS + JS

---
+++ test/test.js
@@ -0,0 +1,5 @@
+document.getElementById('comprar').addEventListener('click', function() {
+    Swal.fire('Pedido efetuado com sucesso!', 
+      'Seu pedido foi efetuado com sucesso! aproveite sua leitura 📚', 
+      'success');
+  });
